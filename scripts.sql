ffa8546036adf2ba0df620209dc8474ea88c5094
PIGNATELLI\a3446 <samuel.miralles@inycom.es>
2024-07-24 12:16:09 +0200

    cambios

--- scripts.sql
+++ scripts.sql
@@ -23,3 +23,56 @@ end;
 VERSION
 2.00.072.00.1706869717
 
+--Obtener fecha actual
+SELECT 
+
+--ESCRIBIR LOG
+CALL LIB_GLOBAL_CESCE :w_debug (
+        i_Tenant,
+        'Fichero MOVIMIENTO_CARTERA con '  || to_varchar(numLineasFichero) || ' lineas distintas. Insertando en tabla EXT_MOVIMIENTO_CARTERA_CREDITO_HIST',
+        cReportTable,
+        io_contador
+    );
+
+
+-- LOTES
+DO BEGIN
+    DECLARE BATCH_SIZE INT := 1000;
+    DECLARE OFFSET INT := 0;
+    DECLARE TOTAL_COUNT INT;
+    DECLARE BATCH_ID INT := 1;
+
+    -- Obtener el número total de registros a procesar
+    SELECT COUNT(*) INTO TOTAL_COUNT FROM EXT.EXT_CARGA_MASIVA_HIST;
+
+    WHILE OFFSET < TOTAL_COUNT DO
+        BEGIN
+            DECLARE EXIT HANDLER FOR SQLEXCEPTION
+            BEGIN
+                -- Registrar el error
+               SELECT 'ERROR' FROM DUMMY;
+               -- LEAVE;
+            END;
+
+            -- Procesar un lote de datos
+            SELECT BATCH_ID,ROW_NUMBER() OVER (ORDER BY CREATEDATE ) AS row_num,*
+            FROM EXT.EXT_CARGA_MASIVA_HIST
+            ORDER BY CREATEDATE
+            LIMIT :BATCH_SIZE OFFSET :OFFSET;            
+            
+
+            -- Incrementar el offset para el siguiente lote
+            OFFSET := OFFSET + BATCH_SIZE;
+            -- Incrementar el ID del lote
+            BATCH_ID := BATCH_ID + 1;
+        END;
+    END WHILE;
+    
+    SELECT * FROM EXT.CSE_DEBUG 
+    WHERE 1=1
+    -- AND PROCESO LIKE '%CARGA_MASIVA%' 
+    --AND CAST(DATETIME AS DATE) = CAST('2024-07-12' AS DATE)
+    and text not like '%Insertados%'
+    and datetime >='2024-07-15 11:51:12.073000000'
+    AND TEXT LIKE '%1689_MVTEST_%';
+END;
