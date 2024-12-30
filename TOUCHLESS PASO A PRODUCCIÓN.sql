8813b50b301dd55b78d2875d077e74120e59d0df
PIGNATELLI\a3446 <samuel.miralles@inycom.es>
2024-12-30 13:00:57 +0100

    COMMIT

---
+++ TOUCHLESS PASO A PRODUCCIÓN.sql
@@ -0,0 +1,12 @@
+/********************** PASO A PRODUCCIÓN TOUCHLESS *************/
+
+-- RUTA INSTALACIÓN TOUCHLESS
+C:\Users\a3446\TouchlessDeployment\TD_Client\bin
+
+
+1. PROBAR CONEXIÓN
+TDClient.cmd -o testConnection
+
+2.
+TDClient.cmd -o update -m ChangeLogMaster.xml -d "C:\Users\a3446\OneDrive - INSTRUMENTACION Y COMPONENTES SA\CESCE-AAGeneral\08.TouchlessDeployment\SQL_CESCE_20241218"
+
